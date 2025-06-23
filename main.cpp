7b84f14e08109b46267247cf86020298224adfb0
Atos Singh <sheemaratos@gmail.com>
2025-06-23 18:29:09 +0200

    initial gui + copy feature

--- main.cpp
+++ main.cpp
@@ -1,58 +1,96 @@
 #include <wx/wx.h>
+#include <wx/clipbrd.h>
 #include "include/base64.h"
- 
+enum
+{
+    ID_Hello = 1
+};
+
+
 class MyApp : public wxApp
 {
 public:
     bool OnInit() override;
 };
- 
+
 wxIMPLEMENT_APP(MyApp);
- 
+
+
+
 class MainFrame : public wxFrame
 {
 public:
     MainFrame();
  
 private:
-    void OnHello(wxCommandEvent& event);
     void OnExit(wxCommandEvent& event);
     void OnAbout(wxCommandEvent& event);
 };
  
-enum
-{
-    ID_Hello = 1
-};
+
+
+
  
 bool MyApp::OnInit()
 {
     MainFrame *frame = new MainFrame();
     frame->Show(true);
     frame->SetSize(wxSize(800, 600));
+    frame->Center();
+    frame->SetMaxSize(wxSize(800, 600));
+    frame->SetMinSize(wxSize(800, 600));
     return true;
 }
+
+
+
  
-MainFrame::MainFrame()
-    : wxFrame(nullptr, wxID_ANY, "Hello World")
+MainFrame::MainFrame() : wxFrame(nullptr, wxID_ANY, "Base64 Encoder/Decoder")
 {
-    wxMenu *menuFile = new wxMenu;
-    menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
-                     "Help string shown in status bar for this menu item");
-    menuFile->AppendSeparator();
-    menuFile->Append(wxID_EXIT);
  
     wxMenu *menuHelp = new wxMenu;
     menuHelp->Append(wxID_ABOUT);
+    menuHelp->Append(wxID_EXIT, "&Exit");
  
     wxMenuBar *menuBar = new wxMenuBar;
-    menuBar->Append(menuFile, "&File");
     menuBar->Append(menuHelp, "&Help");
  
     SetMenuBar( menuBar );
+    
+    wxArrayString choices;
+    choices.Add("Decode");
+    choices.Add("Encode");
 
+    wxPanel *panel = new wxPanel(this, wxID_ANY);
+    wxTextCtrl *textctrl = new wxTextCtrl(panel, wxID_ANY, "", wxPoint(20, 20), wxSize(740, 50), wxTE_MULTILINE);
+    wxChoice *choice = new wxChoice(panel, wxID_ANY, wxPoint(350, 80), wxSize(100, 30), choices);
+    wxButton *button = new wxButton(panel, wxID_ANY, "Process", wxPoint(520, 80), wxSize(100, 30));
+    wxButton *cpybutton = new wxButton(panel, wxID_ANY, "Copy", wxPoint(660, 80), wxSize(100, 30));
+    wxStaticText *resultLabel = new wxStaticText(panel, wxID_ANY, "Result:", wxPoint(20, 120), wxDefaultSize, 
+    wxALIGN_LEFT, "resultLabel");
+    
+    button->Bind(wxEVT_BUTTON, [textctrl, choice, choices, resultLabel](wxCommandEvent& event) {
+        wxString input = textctrl->GetValue();
+        wxString result = "";
+        if (choice->GetSelection() == choices.Index("Decode")) {
+            result = base64_decode(input.ToStdString());
+            
+        } else if (choice->GetSelection() == choices.Index("Encode")) {
+            result = base64_encode(input.ToStdString());
+        }
+        resultLabel->SetLabel("Result: " + result);
+    });
 
-    Bind(wxEVT_MENU, &MainFrame::OnHello, this, ID_Hello);
+    cpybutton->Bind(wxEVT_BUTTON, [resultLabel](wxCommandEvent& event) {
+        wxString result = resultLabel->GetLabel().AfterFirst(':').Trim(true).Trim(false);
+        if (wxTheClipboard->Open()) {
+            wxTheClipboard->SetData(new wxTextDataObject(result));
+            wxTheClipboard->Close();
+        }
+    });
+
+    choice->SetSelection(1);
+
     Bind(wxEVT_MENU, &MainFrame::OnAbout, this, wxID_ABOUT);
     Bind(wxEVT_MENU, &MainFrame::OnExit, this, wxID_EXIT);
 }
@@ -64,11 +102,7 @@ void MainFrame::OnExit(wxCommandEvent& event)
  
 void MainFrame::OnAbout(wxCommandEvent& event)
 {
-    wxMessageBox("This is a wxWidgets Hello World example",
-                 "About Hello World", wxOK | wxICON_INFORMATION);
+    wxMessageBox("This is a simple wxWidgets application that encodes and decodes Base64 strings made just for fun and try wxWidgets.\nI used Ai to help me with CmakeLists.txt.",
+                 "About", wxOK | wxICON_INFORMATION);
 }
  
-void MainFrame::OnHello(wxCommandEvent& event)
-{
-    wxMessageBox("Hello world from wxWidgets!", "Hello World");
-}
